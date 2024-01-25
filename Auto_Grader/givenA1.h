29ad0f9cbe17e227eff69d48311b490aba26f54c
Toaha Ayman Shams <tshams@linux-03.socs.uoguelph.ca>
2024-01-24 21:11:56 -0500

    modified the ReadMe and header

--- Auto_Grader/givenA1.h
+++ Auto_Grader/givenA1.h
@@ -1,20 +1,3 @@
-/*
-Student Name: Toaha Ayman Shams
-Student ID: 1210216
-Due Date: 2, 3, 23
-Course: CIS*2500
-I have exclusive control over this submission via my password.
-By including this header comment, I certify that:
-1) I have read and understood the policy on academic integrity.
-2) I have completed Moodle's module on academic integrity.
-3) I have achieved at least 80% on the academic integrity quiz
-I assert that this work is my own. I have appropriate acknowledged
-any and all material that I have used, be it directly quoted or
-paraphrased. Furthermore, I certify that this assignment was written
-by me in its entirety.
-*/
-
-
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
